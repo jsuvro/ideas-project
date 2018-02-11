154d9c613c0c6fc062448c8d2f7634bd8d779d7e
Suvro J <jsuvro17@gmail.com>
2018-02-11 17:21:49 +0600

    working registration with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 // load schema
@@ -14,16 +15,14 @@ router.get("/registration", (req, res) => {
 });
 
 router.post("/registration", (req, res) => {
+    // checking for error with custom validators
     let error = [];
-
     if (req.body.password.length < 4) {
         error.push({text:"password is less than 5 letters"});
     }
-
     if (req.body.password !== req.body.password2) {
         error.push({text:"passwords do not match"});
     }
-
     if (error.length > 0) {
         res.render("users/registration", {
             error:error,
@@ -33,8 +32,28 @@ router.post("/registration", (req, res) => {
             password2: req.body.password2
         });
     } else {
-        res.send("success");
-    }
+        newUser = new User ({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                    .then(() => {
+                        req.flash("success_msg", "Registration successful!");
+                        res.redirect("/users/login");
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        res.redirect("/users/registration");
+                    })
+            });
+        });
+}  
 });
 
 
